7bf33b4d10015cd6af52d2fc763f99488e0c27b8
Vaibhav Bansal <vaibhav.bansal.eee11@iitbhu.ac.in>
2016-12-25 03:02:01 +0530

    mqtt esp8266.c|

--- MQTTClient-C/src/esp8266/MQTTESP8266.h
+++ MQTTClient-C/src/esp8266/MQTTESP8266.h
@@ -1,5 +1,28 @@
+#ifndef _MQTT_ESP8266_H_
+#define _MQTT_ESP8266_H_
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "lwip/sockets.h"
+#include "lwip/inet.h"
+#include "lwip/netdb.h"
+#include "lwip/sys.h"
+
+typedef struct Timer
+{
+	TickType_t xTicksToWait;
+	TimeOut_t xTimeOut;
+} Timer;
+
+typedef struct Network Network;
+
+struct Network
+{
+	int my_socket;
+	int (*mqttread) (Network*, unsigned char*, int, int);
+	int (*mqttwrite) (Network*, unsigned char*, int, int);
+	void (*disconnect) (Network*);
+};
 
 void TimerInit(Timer*);
 int TimerLeftMS(Timer*);
@@ -8,3 +31,8 @@ void TimerCountdownMS(Timer*, unsigned int);
 void TimerCountdown(Timer*, unsigned int);
 int ESP8266_read(Network*, unsigned char*, int, int);
 int ESP8266_write(Network*, unsigned char*, int, int);
+void ESP8266_disconnect(Network*);
+void NetworkInit(Network*);
+int ConnectNetwork(Network*, char*, int);
+
+#endif
