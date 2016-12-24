8f5f7adc8a4c396a9997760294a573488c1cd449
Vaibhav Bansal <vaibhav.bansal.eee11@iitbhu.ac.in>
2016-12-25 02:04:39 +0530

    esp8266_read

--- MQTTClient-C/src/esp8266/MQTTESP8266.c
+++ MQTTClient-C/src/esp8266/MQTTESP8266.c
@@ -5,6 +5,13 @@
 */
 #include "MQTTESP8266.h"
 
+/*Initialize Timer*/
+void TimerInit(Timer* timer)
+{
+	timer->xTicksToWait = 0;
+	memset(&timer->xTimeOut, '\0', sizeof(timer->xTimeOut));
+}
+
 int TimerLeftMS(Timer* timer)
 {
 	xTaskCheckForTimeOut(&timer->xTimeOut, &timer->xTicksToWait); /* updates xTicksToWait to the number left */
@@ -22,7 +29,32 @@ void TimerCountdownMS(Timer* timer, unsigned int timeout_ms)
 	vTaskSetTimeOutState(&timer->xTimeOut); /* Record the time at which this function was entered. */
 }
 
-void TimerCountdown(Timer* timer, unsigned int timeout) 
+void TimerCountdown(Timer* timer, unsigned int timeout)
 {
 	TimerCountdownMS(timer, timeout * 1000);
+}
+
+int ESP8266_read(Network* n, unsigned char* buffer, int len, int timeout_ms)
+{
+  int recvLen = 0;
+  struct timeval tv;
+  tv.tv_sec = timeout_ms/1000;
+  tv.tv_usec = 0;
+  fd_set readset;
+
+  FD_ZERO(&readset);
+  FD_SET(n->my_socket,&readset);
+
+  if (lwip_select(n->my_socket + 1, &readset, NULL, NULL, &tv) > 0 && FD_ISSET(n->my_socket, &readset)) {
+			rccvLen = lwip_recv(n->my_socket, buffer + recvLen, len - recvLen, 0);
+	} else {
+    return -1;
+  }
+  
+	return recvLen;
+}
+
+int ESP8266_write(Network* n, unsigned char* buffer, int len, int timeout_ms)
+{
+
 }
--- MQTTClient-C/src/esp8266/MQTTESP8266.h
+++ MQTTClient-C/src/esp8266/MQTTESP8266.h
@@ -1,8 +1,10 @@
 #include "freertos/FreeRTOS.h"
-#include "freertos/semphr.h"
 #include "freertos/task.h"
 
+void TimerInit(Timer*);
 int TimerLeftMS(Timer*);
 char TimerIsExpired(Timer*);
 void TimerCountdownMS(Timer*, unsigned int);
 void TimerCountdown(Timer*, unsigned int);
+int ESP8266_read(Network*, unsigned char*, int, int);
+int ESP8266_write(Network*, unsigned char*, int, int);
